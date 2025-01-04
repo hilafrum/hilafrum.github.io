db4d29eb220e42edbe539b8fda29db2e0f73a622
hilafrum <hilafrum1321@gmail.com>
2025-01-04 23:23:39 +0200

    Add files via upload

---
+++ twist/task 8/script.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+function backtostart() {
+    document.getElementById("dorothy").style.left = "0px";
+    document.getElementById("dorothy").style.top = "0px";
+    document.getElementById("rs").style.display = "none";
+ 
+ }
+ 
+ 
+ function divClicked() {
+    document.getElementById("dorothy").style.left = "300px";
+    document.getElementById("dorothy").style.top = "400px";
+    document.getElementById("rs").style.display = "inline";
+    
+    
+ }
